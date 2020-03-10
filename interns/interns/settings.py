098b2c3d1bb61b2470c04764b572149afb6a13f4
sukhjit <sukhjit@mantralabsglobal.com>
2020-03-10 14:33:12 +0530

    allowed host

--- interns/interns/settings.py
+++ interns/interns/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '8vl-xw*2wf#kql+0@2bx(oeo-fvd7e0+qevy%@i**jv&es2d3q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
